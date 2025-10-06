0d63590c7654c4688eff6474c0e74794d58ecfb0
Wangchen <3336805422@qq.com>
2025-10-06 16:53:21 +0800

    完成stage3.1和3.2

--- egs/3dspeaker/speaker-diarization/debug_local.sh
+++ egs/3dspeaker/speaker-diarization/debug_local.sh
@@ -2,7 +2,7 @@
 source activate 3D-Speaker
 
 data_root=/f/data/tv_series_plus/tv_data
-tv_name="debug" # "the big bang theory", "I love my family"
+tv_name="debug_local" # "the big bang theory", "I love my family"
 language="en" # 语言类型，支持 "en" 和 "zh-cn"
 
 bash run_video.sh --stage 3 --tv_name $tv_name --language $language --data_root $data_root
