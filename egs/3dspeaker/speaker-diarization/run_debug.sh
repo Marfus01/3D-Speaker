2fa29d54cd40e607ddf8c28ba037019c67575183
Marfus01 <3336805422@qq.com>
2025-10-06 20:01:56 +0800

    上传"debug"实验的 speaker embedding

---
+++ egs/3dspeaker/speaker-diarization/run_debug.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+module purge
+module load miniforge/24.1.2 cuda/11.8 gcc/9.3
+source activate 3D-Speaker
+
+data_root="/data/home/scv7387/run/tv_series_plus/dataset"
+tv_name="debug" # "the big bang theory", "I love my family"
+language="en" # 语言类型，支持 "en" 和 "zh-cn"
+
+bash /data/home/scv7387/run/tv_series_plus/3D-Speaker/egs/3dspeaker/speaker-diarization/run_video.sh --stage 3 --tv_name "$tv_name" --language $language --data_root "$data_root"
