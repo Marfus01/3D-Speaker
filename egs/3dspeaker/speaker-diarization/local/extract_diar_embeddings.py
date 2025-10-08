38f38a8e67c8557848e2f5db2f685b07ebb823b4
Marfus01 <3336805422@qq.com>
2025-10-08 11:28:01 +0800

    简化将 3D-speaker 添加到系统路径的代码

--- egs/3dspeaker/speaker-diarization/local/extract_diar_embeddings.py
+++ egs/3dspeaker/speaker-diarization/local/extract_diar_embeddings.py
@@ -19,17 +19,10 @@ import torchaudio
 import torch.distributed as dist
 
 current_file_path = os.path.abspath(__file__)
-project_root = os.path.join(
-    os.path.dirname(current_file_path),  # local/
-    '..', '..', '..', '..'               # 回到 3D-Speaker/ 目录
-)
-project_root = os.path.abspath(project_root)
-speakerlab_path = os.path.join(project_root, 'speakerlab')
-assert os.path.exists(speakerlab_path), f"Cannot find speakerlab directory {speakerlab_path}. Please check the project root path."
-
+# 从'local/'回到'speaker-diarization'目录
+project_root = os.path.abspath(os.path.join(os.path.dirname(current_file_path),'..'))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
-    print(f"Project root added to sys.path: {project_root}")
 
 
 from speakerlab.utils.config import yaml_config_loader, Config
