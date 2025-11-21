fdb9c18af5fddbe577b57302fbc887a7a9930fa7
Marfus01 <3336805422@qq.com>
2025-11-21 14:32:37 +0800

    加载训练参数，分析viterbi过程的bug。发现人脸混淆矩阵设置为对角阵后，人脸解码与观测相同，语音解码比不修改人脸混淆矩阵更差

--- egs/3dspeaker/speaker-diarization/local/cluster_and_postprocess.py
+++ egs/3dspeaker/speaker-diarization/local/cluster_and_postprocess.py
@@ -219,10 +219,33 @@ def labels_nested_hmm_full_smooth(S_hat_onehot, F_hat, X_onehot, S_potential_lis
     start_time = time.time()
     print("训练开始时间:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time)))
     model = NestedHMM_full(n_actors=n_actors, n_iter=100, tol=1e-3, verbose=True)
-    model.fit(S_hat_onehot, F_hat, X_onehot, F_potential_list, B_S_diag_min, B_F_diag_min, lengths)
+    # model.fit(S_hat_onehot, F_hat, X_onehot, F_potential_list, B_S_diag_min, B_F_diag_min, lengths)
     end_time = time.time()
     print("训练结束时间:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(end_time)))
     print("训练耗时:", end_time - start_time, "秒")
+
+    # print("\n=== 模型参数 ===")
+    # print("说话人初始概率 β 的logits：\n", model.beta_)
+    # print("说话人转移矩阵 A_S_ 的logits：\n", model.A_S_)
+    # print("说话人识别混淆矩阵 B_S :\n", model.B_S_)
+    # print("协变量X取值为1对说话人初始状态的影响 η1_ :\n", model.eta1_)
+    # print("协变量X取值为1对说话人转移的影响 η2_ :\n", model.eta2_)
+    # print("初始时刻mid-frame中出现各个角色人脸的概率 α ：\n", model.alpha_)
+    # print("mid-frame人脸出现转移矩阵 A_F_ ：\n", model.A_F_)
+    # print("mid-frame人脸识别混淆矩阵 B_F :\n", model.B_F_)
+    # print("中间帧出现某个角色的人脸对说话人初始状态的影响 γ₁_ :\n", model.gamma1_)
+    # print("中间帧出现某个角色的人脸对说话人转移的影响 γ₂_ :\n", model.gamma2_)
+
+    save_name_part_has_neg1 ='_has_neg1' if flag_has_neg1 else ''
+    save_name_part_B_S = f'_B_S_diagmin={B_S_diag_min}' if B_S_diag_min is not None else ''
+    save_name_part_B_F = f'_B_F_diagmin={B_F_diag_min}' if B_F_diag_min is not None else ''
+    # 使用训练好的模型解码隐藏状态
+    model.load_params(os.path.join(result_dir, f'nested_hmm_full{save_name_part_B_S}{save_name_part_B_F}{save_name_part_has_neg1}.pkl'))
+    face_states_viterbi, speaker_states_viterbi = model.predict(S_hat_onehot, F_hat, X_onehot,
+                                                                F_potential_list, lengths) # viterbi 解码结果
+    # model.save_params(os.path.join(result_dir, f'nested_hmm_full{save_name_part_B_S}{save_name_part_B_F}{save_name_part_has_neg1}.pkl'))
+    np.save(os.path.join(result_dir, f'cluster_results_face_states_obs.npy'), F_hat)
+    np.save(os.path.join(result_dir, f'cluster_results_face_states_viterbi_nested_hmm_full{save_name_part_B_S}{save_name_part_B_F}{save_name_part_has_neg1}.npy'), face_states_viterbi)
 
     print("\n=== 模型参数 ===")
     print("说话人初始概率 β 的logits：\n", model.beta_)
@@ -235,16 +258,6 @@ def labels_nested_hmm_full_smooth(S_hat_onehot, F_hat, X_onehot, S_potential_lis
     print("mid-frame人脸识别混淆矩阵 B_F :\n", model.B_F_)
     print("中间帧出现某个角色的人脸对说话人初始状态的影响 γ₁_ :\n", model.gamma1_)
     print("中间帧出现某个角色的人脸对说话人转移的影响 γ₂_ :\n", model.gamma2_)
-
-    # 使用训练好的模型解码隐藏状态
-    face_states_viterbi, speaker_states_viterbi = model.predict(S_hat_onehot, F_hat, X_onehot,
-                                                                F_potential_list, lengths) # viterbi 解码结果
-
-    save_name_part_has_neg1 ='_has_neg1' if flag_has_neg1 else ''
-    save_name_part_B_S = f'_B_S_diagmin={B_S_diag_min}' if B_S_diag_min is not None else ''
-    save_name_part_B_F = f'_B_F_diagmin={B_F_diag_min}' if B_F_diag_min is not None else ''
-    model.save_params(os.path.join(result_dir, f'nested_hmm_full{save_name_part_B_S}{save_name_part_B_F}{save_name_part_has_neg1}.pkl'))
-    np.save(os.path.join(result_dir, f'cluster_results_face_states_viterbi_nested_hmm_full{save_name_part_B_S}{save_name_part_B_F}{save_name_part_has_neg1}.npy'), face_states_viterbi)
 
     if flag_has_neg1:  # 将说话人的-1标签还原回来
         speaker_states_viterbi[speaker_states_viterbi == n_actors - 1] = -1 
