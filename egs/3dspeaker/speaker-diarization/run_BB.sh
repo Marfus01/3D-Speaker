fd31676de274504eff4c2f990c7683c6f5642de3
Marfus01 <3336805422@qq.com>
2025-12-01 17:07:12 +0800

    修改BB, IL的sh，使unreliable_pp不同

--- egs/3dspeaker/speaker-diarization/run_BB.sh
+++ egs/3dspeaker/speaker-diarization/run_BB.sh
@@ -6,5 +6,6 @@ source activate 3D-Speaker
 data_root="/data/home/scv7387/run/tv_series_plus/dataset"
 tv_name="the big bang theory" # "the big bang theory", "I love my family"
 language="en" # 语言类型，支持 "en" 和 "zh-cn"
+unreliable_pp=5.0  # HMM平滑时，认为不可靠的说话人标签百分比，范围0-100.0
 
-bash /data/home/scv7387/run/tv_series_plus/3D-Speaker/egs/3dspeaker/speaker-diarization/run_video.sh --stage 5 --tv_name "$tv_name" --language $language --data_root "$data_root" --master_port 29667
+bash /data/home/scv7387/run/tv_series_plus/3D-Speaker/egs/3dspeaker/speaker-diarization/run_video.sh --stage 5 --tv_name "$tv_name" --language $language --data_root "$data_root" --master_port 29667 --unreliable_pp $unreliable_pp
