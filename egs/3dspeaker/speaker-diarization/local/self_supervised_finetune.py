f4be13679261fb19ec57438ed8239e7537aec919
Marfus01 <3336805422@qq.com>
2025-12-24 12:07:34 +0800

    exp22: 初始round训练5个epoch，后续训练 3 个

--- egs/3dspeaker/speaker-diarization/local/self_supervised_finetune.py
+++ egs/3dspeaker/speaker-diarization/local/self_supervised_finetune.py
@@ -1424,7 +1424,8 @@ def main():
                 crt_acc_e_face = compute_acc_from_anno(epoch_dir_face, args.face_anno_file, mode='all', modal='face')
                 logger.info(f"Round {round}, Face Fine-tune Epoch {ft_epoch_face}: acc(valid_pseudo): {crt_acc_valid_e_pseudo_face:.4f}, acc: {crt_acc_e_face:.4f}")
                 
-                if crt_acc_valid_e_pseudo_face > best_acc_valid_e_pseudo_face:
+                if (round == 0 and ft_epoch_face == 4) or (round > 0 and ft_epoch_face == 2):
+                # if crt_acc_valid_e_pseudo_face > best_acc_valid_e_pseudo_face:
                     best_acc_valid_e_pseudo_face, best_epoch_face = crt_acc_valid_e_pseudo_face, ft_epoch_face
                     patience_counter_epoch_face = 0
                     # Save best face model
@@ -1445,17 +1446,18 @@ def main():
                                 pickle.dump(best_potential_list_dic_face, f)
                     if world_size > 1:
                         dist.barrier()
-                else:
-                    patience_counter_epoch_face += 1
-                    logger.info(f"Round {round}, Face Fine-tune Epoch {ft_epoch_face}: No improvement. Patience(epoch): {patience_counter_epoch_face}/{args.early_stop_patience_epoch}")
-                    if ft_epoch_face > 2:
-                        if patience_counter_epoch_face >= args.early_stop_patience_epoch:
-                            logger.info(f"Early stopping at face epoch {ft_epoch_face}")
-                            break
-                        if (crt_acc_valid_e_pseudo_face - prev_acc_valid_e_pseudo_face) < -0.05:
-                            logger.info(f"Early stopping at face epoch {ft_epoch_face} due to significant drop: {prev_acc_valid_e_pseudo_face:.4f} -> {crt_acc_valid_e_pseudo_face:.4f}")
-                            break
-                prev_acc_valid_e_pseudo_face = copy.deepcopy(crt_acc_valid_e_pseudo_face)
+                    break
+                # else:
+                #     patience_counter_epoch_face += 1
+                #     logger.info(f"Round {round}, Face Fine-tune Epoch {ft_epoch_face}: No improvement. Patience(epoch): {patience_counter_epoch_face}/{args.early_stop_patience_epoch}")
+                #     if ft_epoch_face > 2:
+                #         if patience_counter_epoch_face >= args.early_stop_patience_epoch:
+                #             logger.info(f"Early stopping at face epoch {ft_epoch_face}")
+                #             break
+                #         if (crt_acc_valid_e_pseudo_face - prev_acc_valid_e_pseudo_face) < -0.05:
+                #             logger.info(f"Early stopping at face epoch {ft_epoch_face} due to significant drop: {prev_acc_valid_e_pseudo_face:.4f} -> {crt_acc_valid_e_pseudo_face:.4f}")
+                #             break
+                # prev_acc_valid_e_pseudo_face = copy.deepcopy(crt_acc_valid_e_pseudo_face)
             
             optimizer_face.zero_grad()
             logger.info(f"Round {round}: Best face fine-tuned epoch={best_epoch_face}, acc(valid_pseudo)={best_acc_valid_e_pseudo_face:.4f}")
