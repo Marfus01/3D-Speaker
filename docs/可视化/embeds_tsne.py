e06e1ac5f3cc018dfb78b739186cdc10ae3007d1
Wangchen <3336805422@qq.com>
2026-01-18 20:09:57 +0800

    update: 在embedding tsne之前加入eer计算

--- docs/可视化/embeds_tsne.py
+++ docs/可视化/embeds_tsne.py
@@ -1,16 +1,10 @@
 import numpy as np
 import pandas as pd
-import os, re, glob, sys, pickle
-from sklearn import manifold
-from sklearn.cluster import KMeans
+import os, pickle, random
+from operator import itemgetter
+from sklearn import manifold, metrics
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-
-
-TV_name = 'I love my family' # 'I love my family', 'the big bang theory'
-modal = 'face' # 'face', 'audio'
-load_from_pretrain = False  # whether to load embeddings from pre-trained model or fine-tuned model
-round_idx = 9  # only used when load_from_pretrain is False
 
 def load_embeddings(TV_name, modal, load_from_pretrain=False, round_idx=0):
     if load_from_pretrain:
@@ -90,14 +84,7 @@ def load_embeddings_ft(file_path):
     embeds_sample_ids = np.array(list(obj.keys()))
     return embeds_arr, embeds_sample_ids
 
-def get_labels(TV_name, modal):
-    if TV_name == 'I love my family':
-        characters_index_dic = {'傅老': 0, '和平': 1, '志新': 2, '志国': 3, '圆圆': 4, '小凡': 5, '小张': 6, '燕红': 7, 'Others': 8}
-    elif TV_name == 'the big bang theory':
-        characters_index_dic = {'Sheldon': 0, 'Leonard': 1, 'Penny': 2, 'Howard': 3, 'Raj': 4, 'Others': 5}
-    else:
-        raise ValueError('TV name not recognized')
-
+def get_labels(TV_name, modal, characters_index_dic):
     if modal == 'audio':
         xls_file = f'/data/home/scv7387/run/tv_series_plus/dataset/{TV_name}/annotation/text_annotated.xlsx'
         df = pd.read_excel(xls_file, sheet_name='Sheet1')
@@ -115,29 +102,188 @@ def get_labels(TV_name, modal):
     
     return labels, keys
 
+def save_testEER(labels, keys, modal, save_path, characters_index_dic):
+    characters_list = list(characters_index_dic.keys())
+    characters_index_dic_reverse = {v: k for k, v in characters_index_dic.items()}
+    # total number of positive and negative pairs
+    positive_data = 3500
+    negative_data = 3500
+
+    keys_index = list(range(len(keys)))
+    sim_label_list, key1_list, key2_list = [], [], []
+    # num of audio/face for each character
+    pos_cnt_dict = {char: 0 for char in characters_list}
+    neg_cnt_dict = {char: 0 for char in characters_list}
+    i = 0
+    while i < positive_data: 
+        random.shuffle(keys_index)
+        j = 1
+        while labels[keys_index[0]] != labels[keys_index[j]]:
+            j += 1
+        sim_label_list.append(1)
+        pos_cnt_dict[characters_index_dic_reverse[int(labels[keys_index[0]])]] += 2
+        key1_list.append(keys[keys_index[0]])
+        key2_list.append(keys[keys_index[j]])
+        i += 1
+
+    i = 0
+    while i < negative_data: 
+        random.shuffle(keys_index)
+        j = 1
+        while labels[keys_index[0]] == labels[keys_index[j]]:
+            j += 1
+        sim_label_list.append(0)
+        neg_cnt_dict[characters_index_dic_reverse[int(labels[keys_index[0]])]] += 1
+        neg_cnt_dict[characters_index_dic_reverse[int(labels[keys_index[j]])]] += 1
+        key1_list.append(keys[keys_index[0]])
+        key2_list.append(keys[keys_index[j]])
+        i += 1
+
+    print(f'num of {modal} for each character in positive pairs:', pos_cnt_dict)
+    print(f'num of {modal} for each character in negative pairs:', neg_cnt_dict)
+    print('total num of positive pairs:', sum(pos_cnt_dict.values())/2)
+    print('total num of negative pairs:', sum(neg_cnt_dict.values())/2)
+
+    with open(save_path, 'w') as f:
+        for i,j,k in zip(sim_label_list, key1_list, key2_list):
+            f.write(str(i)+' '+j+' '+k+'\n')
+
+def read_testEER(file_path):
+    sim_label_list, key1_list, key2_list = [], [], []
+    for line in open(file_path).read().splitlines():
+        items = line.split()
+        sim_label_list.append(int(items[0]))
+        key1_list.append(items[1])
+        key2_list.append(items[2])
+    return sim_label_list, key1_list, key2_list
+
+def tuneThresholdfromScore(scores, labels, target_fa, target_fr = None):
+    fpr, tpr, thresholds = metrics.roc_curve(labels, scores, pos_label=1)
+    fnr = 1 - tpr
+    tunedThreshold = []
+    if target_fr:
+        for tfr in target_fr:
+            idx = np.nanargmin(np.absolute((tfr - fnr)))
+            tunedThreshold.append([thresholds[idx], fpr[idx], fnr[idx]])
+    for tfa in target_fa:
+        idx = np.nanargmin(np.absolute((tfa - fpr))) # np.where(fpr<=tfa)[0][-1]
+        tunedThreshold.append([thresholds[idx], fpr[idx], fnr[idx]])
+    idxE = np.nanargmin(np.absolute((fnr - fpr)))
+    eer  = max(fpr[idxE],fnr[idxE])*100
+
+# Creates a list of false-negative rates, a list of false-positive rates
+# and a list of decision thresholds that give those error-rates.
+def ComputeErrorRates(scores, labels):
+      # Sort the scores from smallest to largest, and also get the corresponding
+      # indexes of the sorted scores.  We will treat the sorted scores as the
+      # thresholds at which the the error-rates are evaluated.
+      sorted_indexes, thresholds = zip(*sorted(
+          [(index, threshold) for index, threshold in enumerate(scores)],
+          key=itemgetter(1)))
+      sorted_labels = []
+      labels = [labels[i] for i in sorted_indexes]
+      fnrs = []
+      fprs = []
+
+      # At the end of this loop, fnrs[i] is the number of errors made by
+      # incorrectly rejecting scores less than thresholds[i]. And, fprs[i]
+      # is the total number of times that we have correctly accepted scores
+      # greater than thresholds[i].
+      for i in range(0, len(labels)):
+          if i == 0:
+              fnrs.append(labels[i])
+              fprs.append(1 - labels[i])
+          else:
+              fnrs.append(fnrs[i-1] + labels[i])
+              fprs.append(fprs[i-1] + 1 - labels[i])
+      fnrs_norm = sum(labels)
+      fprs_norm = len(labels) - fnrs_norm
+
+      # Now divide by the total number of false negative errors to
+      # obtain the false positive rates across all thresholds
+      fnrs = [x / float(fnrs_norm) for x in fnrs]
+
+      # Divide by the total number of corret positives to get the
+      # true positive rate.  Subtract these quantities from 1 to
+      # get the false positive rates.
+      fprs = [1 - x / float(fprs_norm) for x in fprs]
+      return fnrs, fprs, thresholds
+
+# Computes the minimum of the detection cost function.  The comments refer to
+# equations in Section 3 of the NIST 2016 Speaker Recognition Evaluation Plan.
+def ComputeMinDcf(fnrs, fprs, thresholds, p_target, c_miss, c_fa):
+    min_c_det = float("inf")
+    min_c_det_threshold = thresholds[0]
+    for i in range(0, len(fnrs)):
+        # See Equation (2).  it is a weighted sum of false negative
+        # and false positive errors.
+        c_det = c_miss * fnrs[i] * p_target + c_fa * fprs[i] * (1 - p_target)
+        if c_det < min_c_det:
+            min_c_det = c_det
+            min_c_det_threshold = thresholds[i]
+    # See Equations (3) and (4).  Now we normalize the cost.
+    c_def = min(c_miss * p_target, c_fa * (1 - p_target))
+    min_dcf = min_c_det / c_def
+    return min_dcf, min_c_det_threshold
+
+def evaluate_EER(embeds_dict, testEER_file):
+    sim_label_list, key1_list, key2_list = read_testEER(testEER_file)
+    scores = []
+    for k1, k2 in zip(key1_list, key2_list):
+        emb1 = embeds_dict[k1]
+        emb2 = embeds_dict[k2]
+        score = np.dot(emb1, emb2) / (np.linalg.norm(emb1) * np.linalg.norm(emb2))
+        scores.append(score)
+    
+    eer = tuneThresholdfromScore(scores, sim_label_list, [1, 0.1])[1]
+    fnrs, fprs, thresholds = ComputeErrorRates(scores, sim_label_list)
+    minDCF, _ = ComputeMinDcf(fnrs, fprs, thresholds, 0.05, 1, 1)
+    return eer, minDCF
+
+
+
+TV_name = 'I love my family' # 'I love my family', 'the big bang theory'
+modal = 'face' # 'face', 'audio'
+load_from_pretrain = False  # whether to load embeddings from pre-trained model or fine-tuned model
+round_idx = 9  # only used when load_from_pretrain is False
+
+if TV_name == 'I love my family':
+    characters_index_dic = {'傅老': 0, '和平': 1, '志新': 2, '志国': 3, '圆圆': 4, '小凡': 5, '小张': 6, '燕红': 7, 'Others': 8}
+elif TV_name == 'the big bang theory':
+    characters_index_dic = {'Sheldon': 0, 'Leonard': 1, 'Penny': 2, 'Howard': 3, 'Raj': 4, 'Others': 5}
+else:
+    raise ValueError('TV name not recognized')
+
 # load embeddings and labels
 embeds_arr, embeds_sample_ids = load_embeddings(TV_name, modal, load_from_pretrain, round_idx)
-print('Loaded embeddings shape:', embeds_arr.shape)
-# L2 normalize
 embeds_arr = embeds_arr / np.linalg.norm(embeds_arr, axis=1, keepdims=True)
-# use all embeds to get tsne, but only plot labeled ones
+print('Loaded embeddings shape:', embeds_arr.shape)
+labels, useful_keys = get_labels(TV_name, modal, characters_index_dic)
+useful_indexs = np.array([embeds_sample_ids.tolist().index(key) for key in useful_keys])
+print(f'Number of {modal} samples with label: {len(labels)}')
+
+# save testEER file(if not exist), and evaluate EER and minDCF
+testEER_file =f'/data/home/scv7387/run/tv_series_plus/dataset/{TV_name}/annotation/{modal}_testEER.txt'
+if not os.path.exists(testEER_file):
+    save_testEER(labels, useful_keys, modal, testEER_file, characters_index_dic)
+embeds_dict = {embeds_sample_ids[i]: embeds_arr[i] for i in range(embeds_arr.shape[0]) if embeds_sample_ids[i] in useful_keys}
+eer, minDCF = evaluate_EER(embeds_dict, testEER_file)
+print(f'EER: {eer:.2f}%, minDCF: {minDCF:.4f}')
+
+# t-SNE visualization
+## use all embeds to get tsne, but only plot labeled ones
 embeds_arr_2dim = manifold.TSNE(n_components=2,
                                 random_state=0,
                                 init='random',
                                 learning_rate='auto').fit_transform(embeds_arr)
-
-# get labels and corresponding indexes
-labels, useful_keys = get_labels(TV_name, modal)
-useful_indexs = np.array([embeds_sample_ids.tolist().index(key) for key in useful_keys])
-print(f'Number of {modal} samples with label: {len(labels)}')
 embeds_arr_2dim = embeds_arr_2dim[useful_indexs]
 
-# Define a custom order for sorting
+## Define a custom order for sorting
 if TV_name == 'the big bang theory':
     legend_order = ['Sheldon', 'Leonard', 'Penny', 'Howard', 'Rajesh', 'Others']  
 else:
     legend_order = ['Fulao', 'Heping', 'Zhixin', 'Zhiguo', 'Yuanyuan', 'Xiaofan', 'Xiaozhang', 'Yanhong', 'Others']
-# Create legend handles in the specified order
+## Create legend handles in the specified order
 handles = []
 color_list = np.vstack((np.array(plt.get_cmap('tab20').colors), plt.get_cmap('tab20c').colors[-3]))  # Add a color for 'Others'
 for i in range(len(legend_order)):
@@ -146,6 +292,7 @@ for i in range(len(legend_order)):
     else:
         handles.append(mpatches.Patch(color=color_list[i], label=legend_order[i], alpha=0.5))
 
+## Plotting
 point_size=20
 colors = list(map(lambda x: color_list[x] if x < max(labels) else color_list[20], labels))
 fig, ax = plt.subplots(figsize=(5, 5))  # Set the figure's height and width equal
@@ -160,6 +307,7 @@ plt.yticks(np.arange(int((min(embeds_arr_2dim[:, 1]) - 5)/25)*25, int((max(embed
 # plt.legend(handles=handles, bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0., fontsize=15)
 plt.tick_params(labelsize=10)
 
+## Save figure
 save_dir = f'/data/home/scv7387/run/tv_series_plus/3D-Speaker/docs/figs/tsne_v2_all/{TV_name}'
 os.makedirs(save_dir, exist_ok=True)
 exp_type = 'pretrain' if load_from_pretrain else f'ft_round{round_idx}'
---
+++ docs/待办/论文内容待修改(TODO).md
@@ -0,0 +1,52 @@
+## Section2: Related Works
+### 内容
+2.2 Face Detection and Recognition: 新增对于目前使用的SOTA人脸识别模型相关内容的简要介绍
+2.3 Speaker Recognition: 新增对于目前使用的SOTA说话人识别模型相关内容的简要介绍，并明确speaker recognition与speaker diarization的区别
+2.4 Multimodal Learning of Videos: 新增一段对于AVD领域的综述。
+
+## Section3: Data
+### 内容
+3.1 The BigBang Dataset: 补充强调语音片段根据字幕切分，这与VAD中通过voice-activity-detection和handle overlap的功能类似。对于更一般的多模态视频数据集，也可以采用VAD中常用的方式进行语音片段的切分。（也可以放在letter中说）
+### 图表
+1. Figure 3,4 中的(e):  Auto-correlation of characters’ appearance sequence，改为指定角色出现在前一帧时，后一帧也出现的概率。对于The LoveFamily Dataset，需要额外标注一些数据，避免Yanhong的取值为0.（3️⃣）
+
+## Section4: HMM-Assisted Deep Learning for Video Analysis
+### 内容
+4.2 Alignment of Multimodal Information: 换成现有的聚类、对齐方式
+4.3 The Statistical Model for HMM-Assisted Deep Learning: 在Full Model中，引入Active speaker face、语音长度等信息，更新公式和参数
+4.4 Parameter Estimation: 更新所有参数的估计方式
+4.5, 4.6: predicted probability计算及使用可以移除，因为实际训练会使用全部样本
+### 图表
+1. Table 1: Selection of pre-trained deep learning models in this study. 把人脸、语音识别模型换成目前使用的SOTA模型。
+2. Figure 5: 引入协变量信息X
+> 问题0：具体怎么画？是只加在graphical model里，还是也要在流程图里体现？
+
+## Section5: Real Data Analyses
+### 内容
+5.1 Name Recognition and Face Detection: 明确指出人名识别结果仅在整个过程结束后，将簇id映射到人名时使用，不影响簇id acc的评估。
+5.2 Face and Speaker Recognition. 5.3 Improvement on Representation Learning: 重新组织：
+- 5.2部分：介绍不做后续自监督学习时，对比HADL1 与各种 Joint Cluster Baseline的结果。
+> 问题1: 在appendix是否加入消融实验，展示HADL1在利用不同信息时的效果？
+> 问题2: speaker recognition的Baseline较多，除了原来的Cluster Ensemble之外，VAD的结果也都可以比；face recognition的Baseline似乎只有原来的Cluster Ensemble
+> 问题3: Cluster Ensemble的具体做法：原来没有VAD信息，只能用只包含一张人脸的语音-人脸对；现在是仍这样做，还是用语音-active face对？如果用后者，Face recognition就更没有Baseline了
+> 问题4: Integrating Audio, Visual, and Semantic Information for Enhanced
+Multimodal Speaker Diarization on Multi-party Conversation这篇文章给出的约束聚类方法效果不如3D speaker中默认的联合聚类方法。是否要把这两种方法都放进来。
+- 5.3部分：介绍做了自监督学习之后，HADL2, HADLI 相较 HADL1 的提升。从acc和表征学习两个角度展示效果。
+> 之所以想把这两部分拆开，是因为VAD的方法只会根据视觉信号更新说话人标签，人脸标签不变，没法做人脸的自监督。
+### 图表
+1. Table 3: 更新为最新的结果
+2. Figure 6: 更新为最新的结果
+> EER 数值也要更新（1️⃣）
+
+
+## Supplementary Materials
+### 内容
+1. 在S1之前，新增对AVD方法基本流程的更详细介绍
+2. S4 Enhance MS with Contrastive Learning是否需要保留？
+> 问题5: 是否还要把语音模态的对比学习加进来，作为预处理？
+3. S5 Alignment of Multimodal Information: S5.1 换成新的对齐方法，S5.2重命名为将簇id映射到人名的过程，结果不做评估，只在Recognized results中展示
+4. S6 Detailed Calculations for Inferring HADL: 更新为新的模型和参数计算方式
+5. S7.4 Cost and Speed of HADL: 可以删掉，因为耗时方面相较对比学习没有优势
+### 图表
+1. Figure S5, S7: 用新结果重新绘制（2️⃣）
+2. Table S1: Information of samples for evaluation. 重新统计EER所用标注数据的信息（1️⃣）
